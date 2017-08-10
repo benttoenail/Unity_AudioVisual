a1a8394ae25fc8f63d10f97238fe307231c27e65
benttoenail <maxrose8841@gmail.com>
2017-08-10 18:25:59 +0200

    Added camera movement

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour {
+
+	public float rotateSpeed;
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Rotate camera at a steady, steady, pace...
+		transform.Rotate( new Vector3(0, rotateSpeed, 0));
+
+	}
+}
